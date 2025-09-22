c766ec33a217953f1edc1b26f103d76493d58a7c
Pravin Atigre <atigrepravin@gmail.com>
2025-09-22 12:17:24 +0530

    Update top-headlines.js

--- client/src/components/top-headlines/top-headlines.js
+++ client/src/components/top-headlines/top-headlines.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import NewsBox from '../news-box/news-box';
-import NewsApi from '../../api-services/newsapi';
-import s from './top-headlines.module.css';
+import React from "react";
+import NewsBox from "../news-box/news-box";
+import NewsApi from "../../api-services/newsapi";
+import s from "./top-headlines.module.css";
 
 const categories = [
   {
     id: 2,
-    name: 'Business',
-    key: 'business'
+    name: "Business",
+    key: "business",
   },
   {
     id: 3,
-    name: 'Science',
-    key: 'science'
+    name: "Science",
+    key: "science",
   },
   {
     id: 4,
-    name: 'Entertainment',
-    key: 'entertainment'
+    name: "Entertainment",
+    key: "entertainment",
   },
   {
     id: 1,
-    name: 'All',
-    key: ''
+    name: "general",
+    key: "",
   },
   {
     id: 5,
-    name: 'Technology',
-    key: 'technology'
+    name: "Technology",
+    key: "technology",
   },
   {
     id: 6,
-    name: 'Sports',
-    key: 'sports'
+    name: "Sports",
+    key: "sports",
   },
   {
     id: 7,
-    name: 'Health',
-    key: 'health'
-  }
+    name: "Health",
+    key: "health",
+  },
 ];
 
 class TopHeadlines extends React.Component {
@@ -50,9 +50,9 @@ class TopHeadlines extends React.Component {
       totalResults: 0,
       activeCategoryId: 1,
       params: {
-        country: 'in',
-        pageSize: 40
-      }
+        pageSize: 40,
+        category: "general",
+      },
     };
     this.filterBycategory = this.filterBycategory.bind(this);
     this.fetchData = this.fetchData.bind(this);
@@ -65,16 +65,16 @@ class TopHeadlines extends React.Component {
   fetchData(params) {
     const query = params || this.state.params;
     NewsApi.topHeadlines(query)
-      .then(response => {
+      .then((response) => {
         this.setState({
           articles: response.articles,
-          totalResults: response.totalResults
+          totalResults: response.totalResults,
         });
       })
-      .catch(error => console.error('error: ', error))
+      .catch((error) => console.error("error: ", error))
       .finally(() =>
         this.setState({
-          isLoading: false
+          isLoading: false,
         })
       );
   }
@@ -85,8 +85,8 @@ class TopHeadlines extends React.Component {
         {
           activeCategoryId: category.id,
           params: Object.assign({}, this.state.params, {
-            category: category.key
-          })
+            category: category.key,
+          }),
         },
         () => this.fetchData()
       );
@@ -98,7 +98,7 @@ class TopHeadlines extends React.Component {
       <React.Fragment>
         <div className={s.filterButtonsContainer}>
           <ul className={s.filterButtons}>
-            {categories.map(category => {
+            {categories.map((category) => {
               const buttonClasses =
                 this.state.activeCategoryId === category.id
                   ? `${s.filterButtonItem} ${s.active}`
